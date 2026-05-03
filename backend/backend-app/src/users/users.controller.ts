feb834ee7fa4ac5f851883a501265a9985e2aea0
gouthamx67 <gouthamec27@gmail.com>
2026-05-03 21:10:03 +0530

    Save local changes before push

--- backend/backend-app/src/users/users.controller.ts
+++ backend/backend-app/src/users/users.controller.ts
@@ -1,17 +1,19 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
-    constructor(private readonly usersService: UsersService) {}
+  constructor(private usersService: UsersService) {}
 
-    @Get()
-    getUsers() {
-        return this.usersService.getAllUsers();
-    }
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  getUsers() {
+    return this.usersService.getAllUsers();
+  }
 
-    @Post()
-    createUser(@Body() body: any) {
-        return this.usersService.createUser(body);
-    }
+  @Post()
+  createUser(@Body() body: any) {
+    return this.usersService.createUser(body);
+  }
 }
