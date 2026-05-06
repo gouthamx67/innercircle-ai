ea38b10931155d4a4e98b53be2cd6936c8c2f58d
gouthamx67 <gouthamec27@gmail.com>
2026-05-07 00:02:17 +0530

    feat: add conversations module to backend

--- backend/backend-app/src/app.module.ts
+++ backend/backend-app/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { PrismaModule } from './database/prisma/prisma.module';
+import { ConversationsModule } from './conversations/conversations.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, PrismaModule],
+  imports: [UsersModule, AuthModule, PrismaModule, ConversationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
